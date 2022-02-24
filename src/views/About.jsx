dcaa28090956339ff28db4ec9d8d9129b7720ee5
sholom perman <sholomperman@gmail.com>
2022-02-24 14:10:03 +0200

    about page not done

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -5,46 +5,45 @@ const tethers = [
     {
         name: "pesach brake",
         position: 'february 01, 2022, 10:00:00',
-        img: null,
+        img: undefined,
         bio: 'ffdsaf fdsaafds fdsasafdsf fdsfasfdsa',
     },
     {
         name: "pesach brake",
         position: 'february 01, 2022, 10:00:00',
-        img: null,
+        img: undefined,
         bio: 'ffdsaf fdsaafds fdsasafdsf fdsfasfdsa',
     },
     {
         name: "pesach brake",
         position: 'february 01, 2022, 10:00:00',
-        img: null,
+        img: undefined,
         bio: 'ffdsaf fdsaafds fdsasafdsf fdsfasfdsa',
     },
     {
         name: "pesach brake",
         position: 'february 01, 2022, 10:00:00',
-        img: null,
+        img: undefined,
         bio: 'ffdsaf fdsaafds fdsasafdsf fdsfasfdsa',
     },
 ];
-
-const anonymous = 'https://www.tutorsvalley.com/public/storage/uploads/tutor/1574383712-1AB5217C-5A13-4888-A5A1-BE0BCADBC655.png'
 
 const About = () => {
   return (
     <div className="aboutContainer">
       <div className="top">
-        <h1></h1>
-        <p></p>
+        <h1>our teachers and mashpim</h1>
       </div>
       <div className="bottom">
         {
           tethers.map((i) => (
             <div>
-              {i.img ? <img src={i.img}alt = "#" /> : <img src={anonymous}alt = "#" />}
+              {i.img ? <img src={i.img} alt="#" /> : <img src='https://www.tutorsvalley.com/public/storage/uploads/tutor/1574383712-1AB5217C-5A13-4888-A5A1-BE0BCADBC655.png' alt="#" />}
+              <div>
               <h1>{i.name}</h1>
               <h3>{i.position}</h3>
-              <p>{i.bio}</p>
+                <p>{i.bio}</p>
+              </div>
             </div>
           ))
         }
