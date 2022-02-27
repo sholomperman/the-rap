42a55cd5a9b4a94203a22f9a5f41f0b444658f48
sholom perman <sholomperman@gmail.com>
2022-02-27 15:37:51 +0200

    added the donate with paypal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import { Routes, Route } from "react-router-dom";
+import { React, useState, useEffect } from 'react';
 import About from './views/About';
 import Home from './views/Home';
 import Contact from './views/Contact';
 import Calender from './views/Calender';
-import Donate from './views/Donate';
 import Videos from './views/Videos';
 import Apply from './views/Apply';
 import NavBar from './components/NavBar';
+import ServerLink from './views/ServerLink';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [apiHome, setApiHome] = useState()
+  const [apiAbout, setApiAbout] = useState()
+  const [apiCalender, setApiCalendar] = useState()
+  const [apiContact, setApiContact] = useState()
+  
+  const apiLink = 'http://localhost:1337/';
+  
+  const homeApi = () => {
+        try {
+      const apiRout = 'api/home-imegas?populate=*';
+      const api = async () => {
+        const { data } = await axios.get(`${apiLink}${apiRout}`);
+        setApiHome(data.data);
+      };
+      api();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const contactApi = () => {
+    const apiRout = 'api/contact-image?populate=*';
+    try {
+      const api = async () => {
+        const { data } = await axios.get(`${apiLink}${apiRout}`);
+        setApiContact(data);
+      };
+      api();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  const calendarApi = () => {
+      try {
+        const apiRout = 'api/calendar-dates?populate=*'
+        const api = async () => {
+        const { data } = await axios.get(`${apiLink}${apiRout}`);
+        setApiCalendar(data.data);
+      };
+      api();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    homeApi()
+    calendarApi()
+    contactApi()
+      }, []);
+      
   return (
     <div className="App" >
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/Calender" element={<Calender />} />
-        <Route path="/Donate" element={<Donate />} />
+        <Route exact path="/" element={<Home apiHome={apiHome} />} />
+        <Route path="/Calender" element={<Calender apiCalender={apiCalender}/>} />
         <Route path="/Videos" element={<Videos />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/About" element={<About apiAbout={apiAbout}/>} />
+        <Route path="/Contact" element={<Contact apiContact={apiContact}/>} />
         <Route path="/Apply" element={<Apply />} />
+        <Route path="/admin" element={<ServerLink />} />
       </Routes>
     </div>
   );
--- src/views/Calender.jsx
+++ src/views/Calender.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import format from "date-fns/format";
 import getDay from "date-fns/getDay";
 import parse from "date-fns/parse";
@@ -17,28 +18,39 @@ const localizer = dateFnsLocalizer({
     getDay,
     locales,
 });
-const time = '24:00'
-const events = [
-    {
-        title: "pesach brake",
-        start: 'february 01, 2022',
-        end: 'february 10, 2022',
-        startHebrew:"5773 ,כח, שוט",
-        endHebrew:"5773 ,כח, שוט",
-        yeshivaBreak : false,
-    },
-    {
-        title: "return after pesach",
-        start: 'february 10, 2022',
-        end: 'february 15, 2022',
-        startHebrew:"5773 ,כח, שוט",
-        endHebrew:"5773 ,כח, שוט",
-        yeshivaBreak: true,
-    },
-];
+
+// const events = [
+//     {
+//         title: "pesach brake",
+//         start: '2022-02-01',
+//         end: '2022-02-10',
+//         startHebrew:"5773 ,כח, שוט",
+//         endHebrew:"5773 ,כח, שוט",
+//         yeshivaBreak : false,
+//     },
+//     {
+//         title: "return after pesach",
+//         start: 'february 10, 2022',
+//         end: 'february 15, 2022',
+//         startHebrew:"5773 ,כח, שוט",
+//         endHebrew:"5773 ,כח, שוט",
+//         yeshivaBreak: true,
+//     },
+// ];
 
 
-const Calender = () => {
+const Calender = ({ apiCalender }) => {
+  const events = apiCalender?.map((i) => (
+    {
+      title: i.attributes.title,
+      start: i.attributes.startingDateInEnglish,
+      end: i.attributes.endingDateInEnglish,
+      yeshivaBreak: i.attributes.yeshivaBreak
+    }
+  ))
+
+  
+  console.log(events)
   return (
     <>
       <div className="calendarContainer">
@@ -46,41 +58,44 @@ const Calender = () => {
           views={['month']}
           localizer={localizer}
           events={events}
-          startAccessor="start"
-          endAccessor="end"
-        style={{ width: '45%', height: '80%', margin: '60px auto' }}
-        eventPropGetter={(events) => {const backgroundColor = events.yeshivaBreak ? '#177e89' : '#ed2126'; return { style: { backgroundColor } } }}
+          startAccessor='start'
+          endAccessor='end'
+          style={{ width: '45%', height: '80%', margin: '60px auto' }}
+          eventPropGetter={(events) => {const backgroundColor = events.yeshivaBreak ? '#177e89' : '#ed2126'; return { style: { backgroundColor } } }}
         />
       
       
       <div className="calendarAgenda">
         {
-          events.map((i) => (
-            <div>
+          apiCalender?.map((i) => (
+            <div key={i.id}>
               <div className="left">
-                {i.yeshivaBreak
+                {i.attributes.yeshivaBreak
                   
                   ?
 
                   <span
-                    style={{ fontSize: '60px',}}
-                    className="material-icons-round">school</span>
+                    style={{ fontSize: '60px', textAlign: 'center' }}
+                    className="material-icons-round">home
+                  </span>
 
                   :
 
-                    <span
-                    style={{ fontSize: '60px', textAlign: 'center' }}
-                    className="material-icons-round">home</span>}
-                <h1>{i.title}</h1>
+                  <span
+                    style={{ fontSize: '60px', }}
+                    className="material-icons-round">school
+                  </span>}
+
+                <h1>{i.attributes.title}</h1>
               </div>
               <div className="right">
                 <div>
-                  <p>{i.start}</p>
-                  <p>{i.end}</p>
+                  <p>{i.attributes.startingDateInEnglish}</p>
+                  <p>{i.attributes.endingDateInEnglish}</p>
                 </div>
                 <div>
-                  <p>{i.endHebrew}</p>
-                  <p>{i.endHebrew}</p>
+                  <p>{i.attributes.startingDateInHebrew}</p>
+                  <p>{i.attributes.endingDateInHebrew}</p>
                 </div>
                 </div>
             </div>
